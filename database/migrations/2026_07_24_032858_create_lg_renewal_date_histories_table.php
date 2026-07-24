76df385273711e5be3fbf7dda2872fdbef03735c
Ahmed Salah <asalahdev5@gmail.com>
2026-07-24 03:34:52 +0300

    new system

--- database/migrations/2026_07_24_032858_create_lg_renewal_date_histories_table.php
+++ database/migrations/2026_07_24_032858_create_lg_renewal_date_histories_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
             $table->string('renewal_fees_account_bank_statement_odoo_id')->nullable();
             $table->unsignedBigInteger('renewal_fees_journal_entry_id')->nullable();
             $table->unsignedBigInteger('letter_of_guarantee_issuance_id')->index('lg_renewal_foreign');
-            $table->string('renewal_date');
-            $table->decimal('fees_amount', 14);
+            $table->string('renewal_date')->comment('تاريخ التجديد');
+            $table->decimal('fees_amount', 14)->comment('هي عبارة عن المبلغ اللي هيدفعه للبنك علشان يجدد');
             $table->integer('company_id');
             $table->timestamps();
         });
