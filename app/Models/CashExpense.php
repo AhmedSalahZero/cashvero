b889e4d3b1087c430631ac40b9d33e6f85f1a0fe
Ahmed Salah <asalahdev5@gmail.com>
2026-07-29 18:43:59 +0300

    fix code error

--- app/Models/CashExpense.php
+++ app/Models/CashExpense.php
@@ -806,10 +806,11 @@ class CashExpense extends Model  implements IHaveCreditOverdraftStatement
 	{
 		$payableCheque = $this->payableCheque;
 		if($payableCheque){
-				/**
-				 * ! getOdooId Is Not Exists In FinancialInstitution Model
-				 */
-			return $payableCheque->deliveryBank->getOdooId();
+			$financialInstitution = $payableCheque->deliveryBank;
+			$accountTypeId = $payableCheque->account_type;
+			$accountNumber = $payableCheque->account_number;
+
+			return (int) $financialInstitution->getOdooIdForAccount($accountTypeId, $accountNumber);
 		}
 		return 0;
 	}
