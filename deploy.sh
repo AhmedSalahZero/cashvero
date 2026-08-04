7a9826b213a15d5cd5117f98a217ec0165a099e4
Ahmed Salah <asalahdev5@gmail.com>
2026-08-04 15:47:56 +0300

    fix cashvero

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,9 @@ chmod -R 775 bootstrap/cache
 echo ">>> Running migrations"
 /usr/local/bin/ea-php84 artisan migrate --force
 
+echo ">>> Running Sql"
+/usr/local/bin/ea-php84 artisan run:sql
+
 echo ">>> Linking storage"
 /usr/local/bin/ea-php84 artisan storage:link
 
